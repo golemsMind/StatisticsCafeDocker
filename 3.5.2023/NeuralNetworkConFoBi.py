9e2d31d0f47919724bc380ac42d69b22417d3271
KMicha <michael.kahle@yahoo.de>
2023-05-05 16:09:49 +0200

    add action for manual version

--- 3.5.2023/NeuralNetworkConFoBi.py
+++ 3.5.2023/NeuralNetworkConFoBi.py
@@ -145,9 +145,9 @@ for i in range(epochs):
     axs[2, 1].plot(y_test,m_test*y_test+b_test,'r')
     axs[2, 1].set_title('Regression Test')
     if i == (epochs - 1):
-        plt.savefig('Results.jpg')
-    plt.show()
-    time.sleep(1)
+        plt.savefig('results_manual.jpg')
+    #plt.show()
+    #time.sleep(1)
     plt.close(fig)
 
 
